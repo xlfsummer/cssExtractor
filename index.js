045e631fe1f19456c01fdb9bd4521a749cf5ab67
xlfsummer <xlf-summer@qq.com>
2018-03-12 00:06:40 +0800

    完善提取

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
 let cssModule = require('css');
+let path = require('path');
 let f = require('./lib/fs');
 
+/** 是否移除源css声明
+ * @type {boolean}
+*/
+let isUpdateSrc = false;
+let srcDirPath = "./test/project"
+let configPath = "./extract.config.css"
+let outputPath = "./test/extracted.css"
 
 async function main(){
-	let configRules = await getConfigRules("./extract.config.css");
+	let configRules = await getConfigRules(configPath);
 
-	let filePaths = await f.readDirRecursive("./test");
-
-	let extractedRules = [];
-
-	//筛选 css
+	let filePaths = await f.readDirRecursive(srcDirPath);
+	//筛选 css 格式文件
 	filePaths = filePaths.filter(p => /\.css$/.test(p));
 
-	for (let p of filePaths){
-		await processFile(p);
+	/** 筛选出的 css 规则 */
+	let allExtractedRules = [];
+	let extractedContent;
+
+
+	for (let p of filePaths) {
+		let oldContent = await f.readFile(p);
+		let relativePath = path.relative(srcDirPath, p)
+		let { processedContent, extractedRules } = await processFile(oldContent, relativePath);
+
+		if (isUpdateSrc) {
+			await f.writeFile(p, processedContent)
+		}
+		allExtractedRules = allExtractedRules.concat(extractedRules);
 	}
 
-	let extractedContent = cssModule.stringify({
-		stylesheet: { rules: extractedRules }
-	});
+	extractedContent = cssModule.stringify({
+		stylesheet: { rules: allExtractedRules }
+	})
 
-	console.log(extractedContent)
-	f.writeFile('./test/extracted.css', extractedContent);
+	await f.writeFile(outputPath, extractedContent);
 
 	return;
 
-
-
-	async function processFile(path) {
-		let content = await f.readFile(path);
+	async function processFile(content, relativePath) {
 		let css = cssModule.parse(content);
 		let rules = css.stylesheet.rules;
-
-		//add comment to extracted rules
-		extractedRules.push({
-			comment: "extract from " + path,
-			type: "comment",
-		})
+		let processedRules = [];
+		let extractedRules = [];
 	
-		for(let r of rules){
-			if (r.type == "rule") {
-				await processRule(r);
-			} else if(r.type == "comment"){
-				let comment = r.comment
+		for (let r of rules) {
+			switch (r.type) {
+				case "rule":
+					let { processedRule, extractedRule } = await processRule(r);
+					if (processedRule) processedRules.push(processedRule);
+					if (extractedRule) extractedRules.push(extractedRule);
+					break;
+				case "comment":
+					processedRules.push(r);
+					break;
+				default:
+					debugger;
 			}
 		}
 
-		console.log(cssModule.stringify(css));
-		console.log(cssModule.stringify({
-			stylesheet: { rules: extractedRules }
-		}));
+		let processedCss = {
+			stylesheet: { rules: processedRules }
+		};
+
+		if (extractedRules.length) {
+			extractedRules.unshift({
+				comment: "extract from " + relativePath,
+				type: "comment",
+			});
+		}
+
+		let processedContent = cssModule.stringify(processedCss);
+
+		return { processedContent, extractedRules };
 	}
 	
 	/**
@@ -58,41 +84,59 @@ async function main(){
 	 */
 	async function processRule(rule) {
 		let selectors = rule.selectors;
-	
+		let extractedRule , processedRule;
+		let processedDeclar = [];
+		let extractDeclar = [];
+
 		for (let d of rule.declarations) {
 			if (d.type == "declaration") {
-				/** @type {string} */
-				let property = d.property;
+				// /** @type {string} */
+				// let property = d.property;
 		
-				/** @type {string} */
-				let value = d.value;
+				// /** @type {string} */
+				// let value = d.value;
 
-				for (let cr of configRules) {
-					for (let cd of cr.declarations) {
-						if (cd.property == d.property
-							&& cd.value == d.value)
-							extractedRules.push({
-								selectors,
-								declarations: rule.declarations.splice(rule.declarations.indexOf(d), 1),
-								type: "rule"
-							});
-					}
+				if (configRules.some(configRule => testDelaration(configRule, d))) {
+					extractDeclar.push(d)
+				} else {
+					processedDeclar.push(d)
 				}
-
 			} else {
-				debugger;
+				processedDeclar.push(d)
 			}
 		}
+
+		if (extractDeclar.length) {
+			extractedRule = {
+				selectors,
+				declarations: extractDeclar,
+				type: "rule"
+			};
+		}
+
+		processedRule = {
+			selectors,
+			declarations: processedDeclar,
+			type: "rule"
+		};
+
+		return {processedRule, extractedRule};
 	}
 
 };
+
+function testDelaration(rule, declaration) {
+	let declarations = rule.declarations;
+	return declarations
+		.filter(d => d.type == "declaration")
+		.some(d => d.property == declaration.property && d.value == declaration.value);
+}
 
 async function getConfigRules(path) {
 	let content = await f.readFile(path);
 	let cssObj = cssModule.parse(content);
 	return cssObj.stylesheet.rules;
 }
-
 
 (async () => {
 	try { await main(); }
